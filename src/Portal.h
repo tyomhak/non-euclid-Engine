9804e5063814c3303d0b487cd6d3153ffbcf6c56
tyomhak <aghakobyan@gmail.com>
2021-05-10 07:12:28 +0400

    Just adding the Portal class file - more for testing than anything else

---
+++ src/Portal.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#include "Mesh.h"
+
+
+class Portal: public Mesh
+{
+public:
+    Portal()
+    Mesh(.......)
+    {} 
+
+
+private:
+    Mesh mesh;
+    glm::mat4 worldMatrix;
+    
+};
