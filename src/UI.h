ecacb4e57afc7d833ae752d7d605cba670ba09f7
git-felix <60326866+git-felix@users.noreply.github.com>
2021-05-29 02:42:00 +0400

    Updated UI.h object creation window
    Added dropdown menu for choosing the object to be created
    Added tabs for object manipulation before its saving
    Added buttons for Creating object, Saving it to Level, Deleting it from the Level
    
    Tabs are fulfilled by dummy buttons.
    Save and Delete buttons are dummy.
    
    All the dummies need to be added by functionality and some refactoring may take a place.

--- src/UI.h
+++ src/UI.h
@@ -8,7 +8,16 @@
 
 class UI
 {
+private:
+	// attributes of buttons
+	const char* objects_menu_items[4]{ "", "cube", "backpack", "portal" };
+	int selected_object_item = 0;
+	bool tabs_active = true;
+	short tabs_index = 0;
+	ImVec2 cursor_pos = { 0, 250 };	// make this dynamics later to set the cursor position once and forever
+
 public:
+
 	UI(GLFWwindow* _window, Level* _level) :
 		window(_window),
 		level(_level)
@@ -25,14 +34,69 @@ public:
 		//createWindow("Window");
 	}
 
-	void createWindow(const char* name) 
+	void createWindow(const char* window_name) 
 	{
-		ImGui::Begin(name);		
+		// Create a window called "My First Tool", with a menu bar.
+		ImGui::Begin(window_name, &tabs_active, ImGuiWindowFlags_MenuBar);
+
+		// choosing object to be created
+		ImGui::Combo("Choose object", &selected_object_item, objects_menu_items, IM_ARRAYSIZE(objects_menu_items));
+		
+		ImGui::Dummy(ImVec2(0.0f, 7.0f));	// adds horizontal and vertical spacing by mentioned size
+
+		// adding tabs for object manipulations
+		ImGui::Columns(4, false);
+		if (ImGui::Button("Position")) tabs_index = 1;
+		ImGui::NextColumn();
+		if (ImGui::Button("Size")) tabs_index = 2;
+		ImGui::NextColumn();
+		if (ImGui::Button("Color")) tabs_index = 3;
+		ImGui::NextColumn();
+		if (ImGui::Button("Other")) tabs_index = 4;
+		ImGui::Columns(1);
+		ImGui::Separator();		// places a separator line
+
+		if (tabs_index == 1) {
+			// stuff to be displayed in tab 1
+			ImGui::Button("tab1 btn 1");
+			ImGui::Button("tab1 btn 2");
+		}
+		if (tabs_index == 2)
+		{
+			// stuff to be displayed in tab 1
+			ImGui::Button("tab2 btn 1");
+			ImGui::SameLine();
+			ImGui::Button("tab2 btn 2");
+		}
+		// ... add next tabs functionality
+
+		ImGui::SetCursorPos(cursor_pos);	// places upcoming attributes of the window on the place that is set by cursor
+
+		// Save, Delete, Create functionality
+		ImGui::Separator();
+		ImGui::Spacing();ImGui::Spacing();	// can be replaced by ImGui::Dummy(ImVec2(0.0f, 2.0f));
+		if (ImGui::Button("Delete"))
+		{
+			// add functionality to delete added object that is not saved yet
+		}
+
+		ImGui::SameLine();
+		ImGui::Dummy(ImVec2(200.0f, 0.0f));
+		ImGui::SameLine();
 		if (ImGui::Button("Create"))
 		{
-			glm::vec3 position = glm::vec3(-5.0f, 0.0f, 0.0f);
-			level->AddObject("cube", position);
+			glm::vec3 position = glm::vec3(-0.5f, 0.0f, 0.0f);
+			level->AddObject(objects_menu_items[selected_object_item], position);	// add also the 'size' and 'color'
 		}
+
+		ImGui::SameLine();
+		ImGui::Dummy(ImVec2(5.0f, 0.0f));
+		ImGui::SameLine();
+		if (ImGui::Button("Save"))
+		{
+			// add functionality to save the object, i.e. permanently add the object to the level
+		}
+
 		ImGui::End();
 	}
 
@@ -44,7 +108,7 @@ public:
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		createWindow("Window");
+		createWindow("Object Creation/Manipulation window");
 
 
 		// Render dear imgui into screen
