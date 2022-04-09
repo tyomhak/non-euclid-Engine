b277cf9e369282500534bde6399db706463943b2
Artyom <aghakobyan@gmail.com>
2022-04-09 15:44:14 +0400

    Add VS2019 solution to the project.
    - Added a VisualStudio solution file to the project for easier build
    process on Win platform.
    - Moved some files for better project structure.

--- src/ObjHandler.h
+++ src/ObjHandler.h
@@ -14,7 +14,6 @@ class ObjectHandler
 
     static Object GetObject(string type, glm::mat4 worldMatrix = glm::mat4(1.0f))
     {
-        
         if (models.find(type) == models.end())
         {
             string path = "./data/models/" + type + "/" + type + ".obj";
