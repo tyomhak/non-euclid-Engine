07df9a181318583524187e050cb0d3b4c1914d46
ArtyomHakobyan <aghakobyan@gmail.com>
2025-03-21 19:26:05 +0400

    buildable test

---
+++ nonEuclid/src/IncludeHeaders.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// #include "glad/glad.h"
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+// // #include "imgui.h"
+// #include <imgui/imgui.h>
+// #include <imgui_impl_glfw.h>
+// #include <imgui_impl_opengl3.h>
+
+// #include <imgui.h>
